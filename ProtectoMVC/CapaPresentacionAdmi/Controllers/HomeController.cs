fdad9d3efc2c8ab4475c9b6abb3667dc39478ebe
psmedinadi22 <psmedinadi@unal.edu.co>
2023-08-04 17:17:23 -0500

    mvc

--- ProtectoMVC/CapaPresentacionAdmi/Controllers/HomeController.cs
+++ ProtectoMVC/CapaPresentacionAdmi/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace CapaPresentacionAdmi.Controllers
             return View();
         }
 
+        public IActionResult Menu3()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
